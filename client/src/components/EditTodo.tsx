ceabdfb48348837d35ff96c92a76457190dc94ba
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-09-27 23:50:56 +0200

    Update the frontend to use books

--- client/src/components/EditTodo.tsx
+++ client/src/components/EditTodo.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Form, Button } from 'semantic-ui-react'
 import Auth from '../auth/Auth'
-import { getUploadUrl, uploadFile } from '../api/todos-api'
+import { getUploadUrl, uploadFile } from '../api/books-api'
 
 enum UploadState {
   NoUpload,
@@ -12,7 +12,7 @@ enum UploadState {
 interface EditTodoProps {
   match: {
     params: {
-      todoId: string
+      bookId: string
     }
   }
   auth: Auth
@@ -26,7 +26,7 @@ interface EditTodoState {
 export class EditTodo extends React.PureComponent<
   EditTodoProps,
   EditTodoState
-> {
+  > {
   state: EditTodoState = {
     file: undefined,
     uploadState: UploadState.NoUpload
@@ -51,7 +51,7 @@ export class EditTodo extends React.PureComponent<
       }
 
       this.setUploadState(UploadState.FetchingPresignedUrl)
-      const uploadUrl = await getUploadUrl(this.props.auth.getIdToken(), this.props.match.params.todoId)
+      const uploadUrl = await getUploadUrl(this.props.auth.getIdToken(), this.props.match.params.bookId)
 
       this.setUploadState(UploadState.UploadingFile)
       await uploadFile(uploadUrl, this.state.file)
